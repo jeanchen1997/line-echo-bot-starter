e98e5537c6a69e071bf8870616f4e67513b18a48
HP-PC\Jean Chen <jeanchen1997@gmail.com>
2017-07-22 18:36:56 +0800

    qqq

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let bodyParser = require('body-parser')
 let request = require('request')
 let app = express()
 
-const CHANNEL_ACCESS_TOKEN = 'Your_Channel_Access_Token'
+const CHANNEL_ACCESS_TOKEN = 'mYcV9f3gijYSinNIkcWlErCYWsP32BmBh7tIm/7DSg5/qmht1cKQiIqbL3Qn6nhpVjm804lR5D36GOpFaptFmCg6FIVThuJs1BiECTWBJMBrUZ7OGAx4OhStl1RF97rhUMgSrqwQVNbY/4fCRWwXjAdB04t89/1O/w1cDnyilFU='
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,6 +14,12 @@ app.listen(PORT, function () {
 
 // handler receiving messages
 app.post('/', function (req, res) {
+    let body=req.body
+    let text=body.events[0].message.text
+    let replyToken=body.events[0].replyToken
+    sendMessage(replyToken,text)
+    console.log(JSON.stringify(body,null,2))
+    res.send("")
 })
 
 // generic function sending messages
